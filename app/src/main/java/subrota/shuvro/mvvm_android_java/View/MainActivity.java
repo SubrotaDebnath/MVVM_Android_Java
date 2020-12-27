3ae3c7c111ec387f7b2b42847942639316f669df
your-nameSubrota <subrota.shuvro@gmail.com>
2020-12-28 04:49:43 +0600

    check character shown

--- app/src/main/java/subrota/shuvro/mvvm_android_java/View/MainActivity.java
+++ app/src/main/java/subrota/shuvro/mvvm_android_java/View/MainActivity.java
@@ -1,16 +1,102 @@
 package subrota.shuvro.mvvm_android_java.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import subrota.shuvro.mvvm_android_java.R;
+import subrota.shuvro.mvvm_android_java.ViewModel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private MainViewModel viewModel;
+    private TextView inputTV, resultTV;
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        inputTV = findViewById(R.id.inputText);
+        resultTV = findViewById(R.id.resultText);
+    }
+    public void onKeyPress(View view) {
+        switch(view.getId()){
+            case R.id.dot:
+                setInputString(".");
+                Log.i(TAG, "dot Taped");
+                break;
+            case R.id.clear_text:
+                setInputString("c");
+                break;
+            case R.id.oneStepBack:
+                setInputString("b");
+                break;
+            case R.id.divide:
+                setInputString("%");
+                break;
+            case R.id.multiply:
+                setInputString("x");
+                break;
+            case R.id.minus:
+                setInputString("-");
+                break;
+            case R.id.plus:
+                setInputString("+");
+                break;
+            case R.id.equal:
+                setInputString("=");
+                break;
+            case R.id.zero:
+                setInputString("0");
+                break;
+            case R.id.one:
+                setInputString("1");
+                break;
+            case R.id.two:
+                setInputString("2");
+                break;
+            case R.id.three:
+                setInputString("3");
+                break;
+            case R.id.four:
+                setInputString("4");
+                break;
+            case R.id.five:
+                setInputString("5");
+                break;
+            case R.id.six:
+                setInputString("6");
+                break;
+            case R.id.seven:
+                setInputString("7");
+                break;
+            case R.id.eight:
+                setInputString("8");
+                break;
+            case R.id.nine:
+                setInputString("9");
+                break;
+         }
+    }
+    void setInputString(String value){
+        String fullString = inputTV.getText().toString();
+        String inputShow = viewModel.inputString(value, fullString);
+        if (inputShow.length()>0){
+            inputTV.setText(inputShow);
+        }else {
+            inputTV.setText("");
+        }
     }
 }
--- app/src/main/java/subrota/shuvro/mvvm_android_java/ViewModel/MainViewModel.java
+++ app/src/main/java/subrota/shuvro/mvvm_android_java/ViewModel/MainViewModel.java
@@ -1,4 +1,38 @@
 package subrota.shuvro.mvvm_android_java.ViewModel;
 
-public class MainViewModel {
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    private String TAG = "MainViewModel";
+
+    public String inputString(String value, String inputString){
+        Log.i(TAG, "input String= " + inputString);
+        if (value.equals("=")){
+            inputString = inputString;
+        }else if (value.equals("c")){
+            inputString = "";
+        }else if (value.equals("b")){
+            inputString = backOperation(inputString);
+        }else {
+            inputString = inputString+value;
+        }
+        return inputString;
+    }
+
+    private String backOperation(String inputString){
+        Log.i(TAG, "input String trim= " + inputString);
+        if (inputString != null && inputString.length() > 0 ) {
+            inputString = inputString.substring(0, inputString.length() - 1);
+        }else {
+            inputString = "";
+        }
+        return inputString;
+    }
+
 }
