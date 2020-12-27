b4a5ac9fb1816dd863b3dc617ee825ba081a2891
your-nameSubrota <subrota.shuvro@gmail.com>
2020-12-28 03:01:53 +0600

    Package Create

--- app/src/main/java/subrota/shuvro/mvvm_android_java/View/MainActivity.java
+++ app/src/main/java/subrota/shuvro/mvvm_android_java/View/MainActivity.java
@@ -1,8 +1,10 @@
-package subrota.shuvro.mvvm_android_java;
+package subrota.shuvro.mvvm_android_java.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import subrota.shuvro.mvvm_android_java.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/subrota/shuvro/mvvm_android_java/ViewModel/MainViewModel.java
@@ -0,0 +1,4 @@
+package subrota.shuvro.mvvm_android_java.ViewModel;
+
+public class MainViewModel {
+}
